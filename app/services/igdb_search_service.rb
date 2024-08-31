939359da5be52c2d9f64afef722e3c07e77fb786
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-08-30 22:07:00 -0400

    Combine Duplicate Search Results into Single Entities (#7)
    * Add Default Sort to Games
    
    * Combine Duplicate Games into Single Search Result
    
    * .compact?

--- app/services/igdb_search_service.rb
+++ app/services/igdb_search_service.rb
@@ -4,10 +4,32 @@ class IgdbSearchService
   end
 
   def search(query)
-    client.get(:games, fields: "name,platforms", search: query, limit: 35)
+    @search_results = client.get(:games, fields: "name,platforms", search: query, limit: 35)
+    rebuild_games
   end
 
   private
 
-  attr_reader :client
+  # Sometimes we'll get multiple results for the same game.
+  # I'm not sure why the IGDB does this ... possibly for special versions
+  # or something.  For now it's kind of annoying and makes the search results
+  # kind of crappy.  Let's merge them all into a single item; we can undo this
+  # later if there's a need for it.
+  def rebuild_games
+    grouped_results.map do |name, properties|
+      combined_platforms = properties.flat_map(&:platforms).compact.uniq
+
+      OpenStruct.new(
+        id: properties.first.id,
+        name: name,
+        platforms: combined_platforms
+      )
+    end.sort_by(&:name)
+  end
+
+  def grouped_results
+    search_results.group_by(&:name)
+  end
+
+  attr_reader :client, :search_results
 end
