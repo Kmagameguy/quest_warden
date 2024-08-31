ba997552534a5eb0ada0ec5a2a6d27bc930e17ae
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-08-31 15:25:37 -0400

    Add DB Constraint to Genre Name Column

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_31_183938) do
   end
 
   create_table "genres", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
