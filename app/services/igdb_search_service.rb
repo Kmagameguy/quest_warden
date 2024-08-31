570bc3271ac1f59c88c27a35ec685aad8948c9a5
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-08-31 15:23:00 -0400

    Unwind Search Result Combinations This didn't work great.  Sometimes would only end up importing the summary/storyline stuff from things like abandoned projects.  Will need to figure out another approach to managing duplicates

--- app/services/igdb_search_service.rb
+++ app/services/igdb_search_service.rb
@@ -4,32 +4,8 @@ class IgdbSearchService
   end
 
   def search(query)
-    @search_results = client.get(:games, fields: "name,platforms", search: query, limit: 35)
-    rebuild_games
+    client.get(:games, fields: "name,platforms", search: query, limit: 35).sort_by(&:name)
   end
 
-  private
-
-  # Sometimes we'll get multiple results for the same game.
-  # I'm not sure why the IGDB does this ... possibly for special versions
-  # or something.  For now it's kind of annoying and makes the search results
-  # kind of crappy.  Let's merge them all into a single item; we can undo this
-  # later if there's a need for it.
-  def rebuild_games
-    grouped_results.map do |name, properties|
-      combined_platforms = properties.flat_map(&:platforms).compact.uniq
-
-      OpenStruct.new(
-        id: properties.first.id,
-        name: name,
-        platforms: combined_platforms
-      )
-    end.sort_by(&:name)
-  end
-
-  def grouped_results
-    search_results.group_by(&:name)
-  end
-
-  attr_reader :client, :search_results
+  attr_reader :client
 end
