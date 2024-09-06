fbabb3d48bbd1d6517e3ed3ae116740e1a2ebc5c
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-09-05 21:55:02 -0400

    Tweak Sign-Up Flow (#20)
    * Automatically Login New Sign-Ups
    
    * Add a Link Back to Home Page in Nav

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Sign-up successful. Welcome!"
-      redirect_to users_path
+      redirect_to root_path
     else
       flash[:alert] = "Sign up failed.  Try again."
       render :new, status: :unprocessable_entity
