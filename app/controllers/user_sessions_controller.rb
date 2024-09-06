efe54100b45a8fb361024322f48d2f700f29d97c
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-09-05 21:17:57 -0400

    Add Flash Notices For Log (in/out) Actions

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -8,6 +8,7 @@ class UserSessionsController < ApplicationController
 
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
+      flash[:notice] = "You have logged in."
       redirect_to root_path
     else
       flash[:alert] = "Login failed"
@@ -17,6 +18,7 @@ class UserSessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    flash[:notice] = "You have been logged out."
     redirect_to root_path
   end
 end
