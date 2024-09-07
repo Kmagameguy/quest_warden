fe45faffdda3e531d866561b8d11d8e4cd2d2166
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-09-06 21:56:49 -0400

    Add User ID Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :id,
+            presence: true,
+            uniqueness: true,
+            numericality: { only_integer: true }
   validates :name, presence: true, uniqueness: true
 end
