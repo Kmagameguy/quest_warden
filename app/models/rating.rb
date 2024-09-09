d4beeae1e593b5669b00a3c993c21221ddecd96b
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-09-08 21:59:10 -0400

    Don't Allow Ratings of 0.0

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,5 +1,5 @@
 class Rating < ApplicationRecord
-  VALID_VALUES = (0..5).step(0.5).to_a.freeze
+  VALID_VALUES = (0.5..5).step(0.5).to_a.freeze
 
   validates :value, inclusion: { in: VALID_VALUES }
   belongs_to :user
