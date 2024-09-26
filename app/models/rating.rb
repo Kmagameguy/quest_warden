b27464a0d874f84af0f458d4abf9e7c28ffdd1e4
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-09-25 22:13:09 -0400

    Set Default Order for Ratings

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -4,4 +4,10 @@ class Rating < ApplicationRecord
   validates :value, inclusion: { in: VALID_VALUES }
   belongs_to :user
   belongs_to :rateable, polymorphic: true
+
+  default_scope { order(value: :desc) }
+
+  def game
+    rateable if rateable_type == Game.name
+  end
 end
