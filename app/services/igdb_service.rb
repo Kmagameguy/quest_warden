0418301497640b5c48a3cc87d43be369cfa07c8d
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2025-10-22 18:25:58 -0400

    Filter Some Noise From Search Results

--- app/services/igdb_service.rb
+++ app/services/igdb_service.rb
@@ -12,6 +12,10 @@ class IgdbService
   end
 
   def game_search(query)
-    get(:games, fields: "name,platforms", search: query, limit: 500).sort_by(&:name)
+    # Apply some filtering to try and cut out some of the noise that exists in the IGDB...
+    get(:games, fields: "name,game_type.type,game_status.status,platforms", search: query, filter: "where rating >= 1", limit: 500)
+      .reject { |game| game&.game_type&.type&.in?([ "DLC", "Bundle", "Mod", "Port", "Fork", "Pack / Addon", "Update" ]) }
+      .reject { |game| game&.game_status&.status&.in?([ "Cancelled", "Rumored" ]) }
+      .sort_by(&:name)
   end
 end
